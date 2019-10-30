18e2663ffbbdb183463187fd996efd0f07e425bc
Dennis <d_silin@yahoo.com>
2019-10-30 02:51:59 +0100

    fixed some of the longitude bugs

--- groundsim/views.py
+++ groundsim/views.py
@@ -1,5 +1,6 @@
 import json
-from math import floor, fmod, pi, atan, sqrt, sin, fabs, cos, atan2
+import julian
+from math import floor, fmod, pi, atan, sqrt, sin, fabs, cos, atan2, trunc
 from datetime import datetime
 from sgp4.earth_gravity import wgs72, wgs84
 from sgp4.io import twoline2rv
@@ -43,35 +44,31 @@ def parse_tle_lines(tle_line_1, tle_line_2):
     tle_data["revolution_number"] = int(line_2[8][:-1])
     return tle_data
 
-def convert_to_geodetic(tle_data, position):
+def convert_to_geodetic(tle_data, position, date):
     geo_data = {}
-    launch_year = int(tle_data["epoch_date"][0:2])
-    dy = float(tle_data["epoch_date"][2:])
-    if launch_year<57:
-        launch_year = launch_year + 2000
-    elif launch_year>56:
-        launch_year = launch_year + 1900
-    geo_data["jd_year"]= 2415020.5 + (launch_year-1900)*365.0 + floor((launch_year-1900-1)/4)
-    jd = geo_data["jd_year"] + dy - 1.0;
-
-    # calculation of neccessary prerequsites
-    ut = fmod(jd + 0.5, 1.0)
-    t = (jd - ut - 2451545.0) / 36525.0
-    omega = 1.0 + 8640184.812866 / 3155760000.0;
-    gmst0 = 24110.548412 + t * (8640184.812866 + t * (0.093104 - t * 6.2E-6))
-    theta_GMST = fmod(gmst0 + 86400 * omega * ut, 86400) * 2 * pi / 86400
+    # copy the data from SGP4 output
     x = position[0]
     y = position[1]
     z = position[2]
+
     # longitude calculation
-    lon = fmod((atan(y/x) - theta_GMST),2*pi)
-    geo_data["lon"] =  lon*180.0/pi
-    # latitude constellation
+    jd = julian.to_jd(date)
+    ut = fmod(jd + 0.5, 1.0)
+    t = (jd - ut - 2451545.0) / 36525.0
+    omega = 1.0 + 8640184.812866 / 3155760000.0;
+    gmst0 = 24110.54841 + t * (8640184.812866 + t * (0.093104 - t * 6.2E-6))
+    theta_GMST = fmod(gmst0 + 86400* omega * ut, 86400) * 2 * pi / 86400
+    lon = fmod((atan(y/x)-theta_GMST),2*pi)
+    lon = lon*180/pi
+    geo_data["raw_l"] = lon
+    geo_data["lon"] = 180 + fmod(lon, 180)
+
+    # latitude calculation
     lat = atan(z/sqrt(x*x + y*y))
     a = 6378.137
     e = 0.081819190842622
     delta = 1.0
-    while (delta>0.0001):
+    while (delta>0.001):
         lat0 = lat
         c = (a * e * e * sin(lat0))/sqrt(1.0 - e * e * sin(lat0) * sin(lat0))
         lat = atan((z + c)/sqrt(x*x + y*y))
@@ -85,17 +82,16 @@ def convert_to_geodetic(tle_data, position):
 
 def compute_orbit(sat_name, date):
     if date == None:
-        dat1 = datetime.now()
-        formatted = [dat1.year, dat1.month, dat1.day, dat1.hour, dat1.minute, dat1.second]
+        date = datetime.now()
+        formatted = [date.year, date.month, date.day, date.hour, date.minute, date.second]
     else:
         formatted = [int(x) for x in date.split(',')]
     satellite = Satellite.objects.get(satellite_name="ISS (ZARYA)")
-    print (formatted)
     satellite_tle = twoline2rv(satellite.satellite_tle1, satellite.satellite_tle2, wgs72)
     tle_data_details = parse_tle_lines(satellite.satellite_tle1, satellite.satellite_tle2)
 
     position, velocity = satellite_tle.propagate(formatted[0], formatted[1], formatted[2], formatted[3], formatted[4], formatted[5])
-    geo_data = convert_to_geodetic(tle_data_details, position)
+    geo_data = convert_to_geodetic(tle_data_details, position, date)
 
     return_data = {
         "error_code":satellite_tle.error,    # nonzero on error
