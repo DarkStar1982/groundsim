bd391120f01bcbee4ff1838af8b07c96eede8ecc
Dennis <d_silin@yahoo.com>
2020-12-06 00:52:09 +0100

    Added stub files for full simulation code

---
+++ groundsim/mse/sim_astro.py
@@ -0,0 +1,13 @@
+from skyfield.api import EarthSatellite
+
+# calculate orbital vector data from the following
+# - TLE set
+# - time (in UTC)
+def get_orbital_data(tle_data, time_data):
+    # create satellite
+    # get values from satellite
+    # return should include
+    # - ground track position
+    # - osculating orbital elements
+    # - day/night flag
+    return []
