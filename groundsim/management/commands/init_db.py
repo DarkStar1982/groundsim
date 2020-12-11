3865aec566c7927771869aa6047ff95e8c10b95c
Dennis <d_silin@yahoo.com>
2020-12-11 23:45:13 +0100

    Added initial data population command

--- groundsim/management/commands/init_db.py
+++ groundsim/management/commands/init_db.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
-from groundsim.models import Satellite
+from groundsim.models import Satellite, MissionScenario
+from groundsim.mse.utils import mission_timer_to_datetime
 
-satellite_records = [
+SATELLITE_LIST = [
     {
         "satellite_name":"USA224",
         "norad_id":37348,
@@ -15,11 +16,32 @@ satellite_records = [
         "tle_line2":"2 25544  51.6436 190.1749 0002049 117.8257 357.3518 15.49178451259535"
     }
 ]
+
+MISSION_SCENARIOS = [
+    {
+        "scenario_id":0,
+        "start_date":{
+            "year":2020,
+            "month":11,
+            "day":4,
+            "hour":18,
+            "min":40,
+            "sec": 37
+        },
+        "mission_name": "eo_basic",
+        "description": "A basic Earth Observation mission",
+        "initial_setup":"json object here", # TBD!
+        "objectives":"json object here" # TBD!
+    }
+]
+
 def flush_db():
     Satellite.objects.all().delete()
+    MissionScenario.objects.all().delete()
 
 def repopulate_db():
-    for item in satellite_records:
+    # add satellites
+    for item in SATELLITE_LIST:
         try:
             sat = Satellite.objects.get(norad_id=item["norad_id"])
         except Satellite.DoesNotExist:
@@ -29,6 +51,19 @@ def repopulate_db():
         sat.satellite_tle1 = item["tle_line1"]
         sat.satellite_tle2 = item["tle_line2"]
         sat.save()
+    # add missions
+    for item in MISSION_SCENARIOS:
+        try:
+            scenario = MissionScenario.objects.get(scenario_id=item["scenario_id"])
+        except MissionScenario.DoesNotExist:
+            scenario = MissionScenario()
+        scenario.start_date = mission_timer_to_datetime(item["start_date"])
+        scenario.mission_name = item["mission_name"]
+        scenario.description = item["description"]
+        scenario.initial_setup = item["initial_setup"]
+        scenario.objectives = item["objectives"]
+        scenario.save()
+
 
 class Command(BaseCommand):
     help = 'Clear database and repopulate it with default data'
